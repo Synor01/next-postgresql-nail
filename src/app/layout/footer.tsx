9c3fe841326ab1984bfa87a53b88ff6f07c42fd0
UnixShen <captainfontend@gmail.com>
2026-03-26 15:18:06 +0800

    feat: a

--- src/app/layout/footer.tsx
+++ src/app/layout/footer.tsx
@@ -10,13 +10,13 @@ export const Footer = () => {
     const router = useRouter();
     return <footer className="fixed bottom-0 left-0 right-0 max-w-sm mx-auto /80 backdrop-blur-sm border-t border-gray-100 rounded-t-3xl" >
         <div className="flex justify-around items-center h-16">
-            <a className="text-pink-500 flex flex-col items-center gap-1" onClick={() => router.push('/')}>
+            <div className="text-pink-500 flex flex-col items-center gap-1" onClick={() => router.push('/')}>
                 <Home size={24} fill="currentColor" />
-            </a>
+            </div>
             <AddCircleOutline className="w-16 h-16 bg-pink-500 rounded-full text-white text-4xl font-light shadow-lg -mt-8 flex items-center justify-center"/>
-            <a className="text-gray-400 flex flex-col items-center gap-1" onClick={() => router.push(isLogined ? '/auth/signout' : '/auth/signin')}>
+            <div className="text-pink-400 flex flex-col items-center gap-1" onClick={() => router.push(isLogined ? '/auth/signout' : '/auth/signin')}>
                 <User size={24} />
-            </a>
+            </div>
         </div>
     </footer >
 }
