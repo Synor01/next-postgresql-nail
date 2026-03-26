d9c45e3366d9c0bb045653292bb305d9a48b9f6e
UnixShen <captainfontend@gmail.com>
2026-03-26 14:09:58 +0800

    feat: HOOK

--- src/app/layout/footer.tsx
+++ src/app/layout/footer.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Home, User } from "lucide-react";
 import { AddCircleOutline } from "antd-mobile-icons";
 import { useRouter } from "next/navigation";
-import { isLogin } from "@/lib/clientUser";
+import { useIsLogin } from "@/lib/clientUser";
 
 export const Footer = () => {
-    const loginMsg = isLogin();
+    const { isLogined} = useIsLogin();
     const router = useRouter();
     return <footer className="fixed bottom-0 left-0 right-0 max-w-sm mx-auto /80 backdrop-blur-sm border-t border-gray-100 rounded-t-3xl" >
         <div className="flex justify-around items-center h-16">
@@ -14,7 +14,7 @@ export const Footer = () => {
                 <Home size={24} fill="currentColor" />
             </a>
             <AddCircleOutline className="w-16 h-16 bg-pink-500 rounded-full text-white text-4xl font-light shadow-lg -mt-8 flex items-center justify-center"/>
-            <a href="#" className="text-gray-400 flex flex-col items-center gap-1" onClick={() => router.push(loginMsg?.isLogined ? '/auth/signout' : '/auth/signin')}>
+            <a href="#" className="text-gray-400 flex flex-col items-center gap-1" onClick={() => router.push(isLogined ? '/auth/signout' : '/auth/signin')}>
                 <User size={24} />
             </a>
         </div>
--- src/lib/clientUser.ts
+++ src/lib/clientUser.ts
@@ -2,11 +2,11 @@
 import { Session } from "next-auth"
 import { useSession } from "next-auth/react"
 
-export const isLogin = (): {
+export const useIsLogin = (): {
     isLogined: boolean,
     session: Session | null,
 } => {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
   return {
     isLogined: status === "authenticated",
     session,
